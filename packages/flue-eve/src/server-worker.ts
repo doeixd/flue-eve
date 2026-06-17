138c56ea24ceb729584100b3cbe0f925984095a9
Patrick Glenn <doeixd@gmail.com>
2026-06-17 16:38:34 -0400

    chore: fix release validation

--- packages/flue-eve/src/server-worker.ts
+++ packages/flue-eve/src/server-worker.ts
@@ -1,2 +1,15 @@
-export { createEveWorkerApp } from "@flue-eve/compat-server/worker";
-export type { EveWorkerBindings, EveWorkerOptions } from "@flue-eve/compat-server/worker";
+export {
+  createCloudflareKvJournalPersistence,
+  createCloudflareKvJournalStore,
+  createDurableObjectJournalPersistence,
+  createDurableObjectJournalRpcPersistence,
+  createEveWorkerApp,
+  resolveWorkerAdmission,
+} from "@flue-eve/compat-server/worker";
+export type {
+  CloudflareDurableObjectStorage,
+  CloudflareKvNamespace,
+  DurableObjectJournalStub,
+  EveWorkerBindings,
+  EveWorkerOptions,
+} from "@flue-eve/compat-server/worker";
